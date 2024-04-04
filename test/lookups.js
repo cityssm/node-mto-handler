e5a52c2ef71c987b0a1863f7b427571742959ea5
Dan Gowans <dangowans@users.noreply.github.com>
2024-04-04 12:48:44 -0400

    test lookups

---
+++ test/lookups.js
@@ -0,0 +1,16 @@
+import assert from 'node:assert';
+import { vehicleColors, vehicleMakes } from '../index.js';
+describe('Exported lookups', () => {
+    const vehicleMakeValues = ['CHEV', 'FORD', 'HYUN'];
+    for (const vehicleMake of vehicleMakeValues) {
+        it(`Includes popular vehicle make: ${vehicleMake}`, () => {
+            assert.ok(Object.keys(vehicleMakes).includes(vehicleMake));
+        });
+    }
+    const vehicleColorValues = ['BLACK', 'BLUE', 'WHITE'];
+    for (const vehicleColor of vehicleColorValues) {
+        it(`Includes popular vehicle color: ${vehicleColor}`, () => {
+            assert.ok(Object.values(vehicleColors).includes(vehicleColor));
+        });
+    }
+});
